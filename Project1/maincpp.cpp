0683dc166061b686ae93b087326531aa14ec5b11
123123 <213213>
2024-12-16 18:41:20 +0300

    fdgfrfufkhjuk

--- Project1/maincpp.cpp
+++ Project1/maincpp.cpp
@@ -26,96 +26,96 @@ int main()
 	std::cout << one - two << "\n";
 	std::cout << one * two << "\n";
 	std::cout << one / two << "\n";
-	*/	
-/*double расстояние , время;
-	float скорость;
-	
-	std::cout <<  "введите расстояние \n";
-	std::cin >> расстояние;
-	std::cout << "введите время \n";
-	std::cin >> время;
-	std::cout << " скорость равна " <<расстояние/время  ;
-
-	double времян, времяк; 
-	int стоимость;
-	int a1, a2, a3, b1 , b2, b3;
-	std::cout << "стоимость одной минуты поездки равна 2 гривна \n";
-	std::cout << "введите  часы начала использования \n";
-	std::cin >> a1;
-	std::cout << "введите  минуты начала использования \n";
-	std::cin >> a2;
-	std::cout << "введите  секунды начала использования \n";
-	std::cin >> a3;
-	std::cout << "введите время завершения использования: часы минуты секунды  \n";
-	std::cin >> b1;
-	std::cin >> b2;
-	std::cin >> b3;
-	std::cout << "стоимость поездки равна: " << (a1 * 60 + a2 + a3 / 60); */
-
-	/*int  a;
-	std::cin >> a ;
-	if (a > 0)
-	{
-		std::cout << "привет";
-	}
-	else if (a < 0)
-	{ 
-		std::cout << "пока";
-	}
-	else if (a == 0)
-	{
-		std::cout << "повар";
-	}
 	*/
-	/*double one, two;
-	char znak;
+	/*double расстояние , время;
+		float скорость;
 
-	one = 0;
-	two = 0;
-	std::cout << "введите первое число:" << "\n";
-	std::cin >> one;
-	std::cout << "введите + - * или /" << "\n";
-	std::cin >> znak;
-	
-	std::cout << "введите второе число:" << "\n";
-	std::cin >> two;
-	if (two==0)
-		std::cout << "на ноль делить нельзя\n ";
-	 if (znak == '+')
-	{
-		std::cout << "сумма " << one + two << "\n";
+		std::cout <<  "введите расстояние \n";
+		std::cin >> расстояние;
+		std::cout << "введите время \n";
+		std::cin >> время;
+		std::cout << " скорость равна " <<расстояние/время  ;
 
-	}
-	else if (znak == '-')
-	{
-		std::cout << "разность " << one - two << "\n";
+		double времян, времяк;
+		int стоимость;
+		int a1, a2, a3, b1 , b2, b3;
+		std::cout << "стоимость одной минуты поездки равна 2 гривна \n";
+		std::cout << "введите  часы начала использования \n";
+		std::cin >> a1;
+		std::cout << "введите  минуты начала использования \n";
+		std::cin >> a2;
+		std::cout << "введите  секунды начала использования \n";
+		std::cin >> a3;
+		std::cout << "введите время завершения использования: часы минуты секунды  \n";
+		std::cin >> b1;
+		std::cin >> b2;
+		std::cin >> b3;
+		std::cout << "стоимость поездки равна: " << (a1 * 60 + a2 + a3 / 60); */
 
-	}
-	else if (znak == '*')
-	{
-		std::cout << "умножение " << one * two << "\n";
+		/*int  a;
+		std::cin >> a ;
+		if (a > 0)
+		{
+			std::cout << "привет";
+		}
+		else if (a < 0)
+		{
+			std::cout << "пока";
+		}
+		else if (a == 0)
+		{
+			std::cout << "повар";
+		}
+		*/
+		/*double one, two;
+		char znak;
 
-	}
-	else if (znak == '/')
-	{
-		 if (two == 0)
-			 std::cout << "на ноль делить нельзя\n ";
-		 else std::cout << "деление " << one / two << "\n";
+		one = 0;
+		two = 0;
+		std::cout << "введите первое число:" << "\n";
+		std::cin >> one;
+		std::cout << "введите + - * или /" << "\n";
+		std::cin >> znak;
 
-	}
-	else {
-		std::cout << "неверный символ\n ";
-	}
-	 if (znak =='/' &&two !=0){}
-	 if(one < 0 ||one  > 10 ){}
-	
-		 
+		std::cout << "введите второе число:" << "\n";
+		std::cin >> two;
+		if (two==0)
+			std::cout << "на ноль делить нельзя\n ";
+		 if (znak == '+')
+		{
+			std::cout << "сумма " << one + two << "\n";
+
+		}
+		else if (znak == '-')
+		{
+			std::cout << "разность " << one - two << "\n";
+
+		}
+		else if (znak == '*')
+		{
+			std::cout << "умножение " << one * two << "\n";
+
+		}
+		else if (znak == '/')
+		{
+			 if (two == 0)
+				 std::cout << "на ноль делить нельзя\n ";
+			 else std::cout << "деление " << one / two << "\n";
+
+		}
+		else {
+			std::cout << "неверный символ\n ";
+		}
+		 if (znak =='/' &&two !=0){}
+		 if(one < 0 ||one  > 10 ){}
 
 
 
 
 
-*/
+
+
+	*/
 	/*
 	float dollar, euro, yuan, jena, farit;
 	float valuta, rubles;
@@ -124,17 +124,17 @@ int main()
 	yuan = 13.6;
 	jena = 0.66;
 	farit = 37;
-	
+
 	std::cout << "Программа Конвертер валют" << "\n";
 	std::cout << "  У нас можно купить доллары , евро , юани , йены, фариты" << "\n";
-	std::cout << "ЦЕНЫ:" << "\n"<< "ЦЕНЫ:\n" << "доллар = 99 рублей\n " << "евро = 106 рублей\n " << "юань = 13,6  рублей \n " 
+	std::cout << "ЦЕНЫ:" << "\n"<< "ЦЕНЫ:\n" << "доллар = 99 рублей\n " << "евро = 106 рублей\n " << "юань = 13,6  рублей \n "
 		<< "йена = 0,66  рубля \n " << "фарит = 37 рублей \n ";
 	std::cout << "Банк берёт 5% комиссию за услугу!" << "\n";
 	std::cout << " сколько валюты хотите приобрести?" << "\n";
 	std::cin >> rubles;
 	std::cout << "Выберите валюту 1 - dollar, 2 -  euro,3 - yuan, 4 - jena,5 -  farit " << "\n";
 	std::cin >> valuta ;
-	
+
 	if (valuta == 1)
 	{
 		std::cout  << dollar*rubles* 0.95 << " рублей с учётом комиссии\n";
@@ -173,35 +173,170 @@ int main()
 		{
 			continue;
 
-			break; 
+			break;
 		}
 		std::cout << " lol ";
 	}
 
 	}*/
-int a = 0;
-while (a < 5)
-	
+	/*int a = 0;
+	while (a < 5)
+
 	{
 	std::cout << " 1 ";
 	a++;
-	} 
-for (int i = 0; i < 5; i++)
+	}
+	for (int i = 0; i < 5; i++)
 
 
-do{}
-	while (true); 
-/*	for each (object var in collection_to_loop)*/
-	
+	do{}
+	while (true); */
+	/*	for each (object var in collection_to_loop)*/
+
+	/*int a = 0;
+	int b=0;
+	std::cin >> a;
+	while (true)
+	{ std::cin >> b;
+	a = b + a;
+	if (b == 0)
+	{
+		std::cout << a;
+		break;
 
 
+	}
+	}*/
+	/*float s1, s2, ves, k = 0, s, nado;
+	float bak = 300, ostatok = 0, maxdistanse = 0;
+	while (true)
+	{
+		std::cout << "введите вес груза";
+		std::cin >> ves;
+		if (ves >= 0 && ves <= 2000)
+		{
+			break;
+		}
+		else { std::cout << " недопустимый вес груза , минимум 0 , максимум 2000"; }
+	}
+	if (ves <= 500 && ves >= 0) {
+		k = 1;
+	}
+	else if (ves <= 1000 && ves >= 500) {
+		k = 4;
+		
+	}
+	else if (ves <= 1500 && ves >= 1000) {
+		k = 7;
+		
+
+		
+	}
+	else if (ves <= 2000 && ves >= 1500)
+	{
+		k = 9;
+		
+	}
+	maxdistanse = bak / k;
+	while (true)
+	{
+		std::cout << "введите расстояние от А до Б";
+		std::cin >> s1;
+		std::cout << "введите расстояние от Б до С";
+		std::cin >> s2;
+		if (s1 >= 0 && s2 >= 0 && s1 <= maxdistanse && s2 <= maxdistanse)
+		{
+			break;
+		}
+		else {
+			std::cout << "недопустимое расстояние! минимум - 0 б максимум\n" << maxdistanse;
 
 
-	
+		}
+
+		s = s1 + s2;
+
+		if (ves <= 500 && ves >= 0) {
+			k = 1;
+			std::cout << "топлива понадобиться лт пункта А до пункта Б\n " << s1 * k;
+			ostatok = bak - ves / s1 * k;
+			std::cout << "топлива останется \n" << ostatok;
+			std::cout << "топлива понадобиться лт пункта Б до пункта С\n " << s2 * k;
+			std::cout << "топлива понадобиться дозоправить\n " << s2 * k - ostatok;
+		}
+		else if (ves <= 1000 && ves >= 500) {
+			k = 4;
+			std::cout << "топлива понадобиться лт пункта А до пункта Б \n" << s1 * k;
+			ostatok = 300 - ves / s1 * k;
+			std::cout << "топлива останется \n" << ostatok;
+			std::cout << "топлива понадобиться лт пункта Б до пункта С \n" << s2 * k;
+			std::cout << "топлива понадобиться дозоправить \n" << s2 * k - ostatok;
+		}
+		else if (ves <= 1500 && ves >= 1000) {
+			k = 7;
+			std::cout << "топлива понадобиться лт пункта А до пункта Б\n" << s1 * k;
+
+			ostatok = 300 - ves / s1 * k;
+			std::cout << "топлива останется \n" << ostatok;
+			std::cout << "топлива понадобиться лт пункта Б до пункта С\n" << s2 * k;
+			std::cout << "топлива понадобиться дозоправить \n" << s2 * k - ostatok;
+		}
+		else if (ves <= 2000 && ves >= 1500)
+		{
+			k = 9;
+			std::cout << "топлива понадобиться лт пункта А до пункта Б\n" << s1 * k;
+			ostatok = 300 - ves / s1 * k;
+			std::cout << "топлива останется \n " << ostatok;
+			std::cout << "топлива понадобиться лт пункта Б до пункта С\n" << s2 * k;
+
+			std::cout << "топлива понадобиться дозоправить\n " << s2 * k - ostatok;
+		}
 
 
+	}
+	*/
 
+std::cout << "                      решение кв уравнений               ";
 
+std::cout << "  ax^2+bx=c=0   ";
+
+float a, b, c, d, x1, x2;
+
+std::cout << "введите A:    ";
+
+std::cin >> a;
+
+std::cout << "введите B:    " ;
+
+std::cin >> b;
+
+std::cout << "введите C:    " ;
+
+std::cin >> c;
+
+std::cout << a << "x^2+"<< b << "х+"<<c <<"=0  ";
+
+d = std::pow(b, 2)-(4*a*c);
+
+std::cout << a << "дискриминант: " << d << "";
+
+if (d<0)
+{
+	std::cout << "корней нет";
+}
+
+else if (d==0)
+{
+	x1 = -b / (2 * a);
+	std::cout << "ваш корень равен:"<< x1<<"  ";
+}
+else 
+{
+	x1 = (-b - std::sqrt(d)) / (2 * a);
+	x2 = (-b + std::sqrt(d)) / (2 * a);
+	std::cout << "ваш корень x1 равен:" << x1 << "  ";
+	std::cout << "ваш корень x2 равен:" << x2 << "  ";
+}
 
 
 
